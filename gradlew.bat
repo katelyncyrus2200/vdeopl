0d54254f5284b858742ce8911a0d0889ca8ee7bb
Katelyn Cyrus <katelyncyrus2200@gmail.com>
2026-01-21 15:37:57 -0800

    VideoRental-style big center control

--- gradlew.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set DIR=%~dp0
-set JAVA_CMD=%JAVA_HOME%\bin\java.exe
-if exist "%JAVA_CMD%" (
-  "%JAVA_CMD%" -jar "%DIR%gradle\wrapper\gradle-wrapper.jar" %*
-) else (
-  java -jar "%DIR%gradle\wrapper\gradle-wrapper.jar" %*
-)
