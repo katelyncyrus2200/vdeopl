5fa981ceccdcec408115e7ca8dbd974d51cb6ea9
Katelyn Cyrus <katelyncyrus2200@gmail.com>
2026-01-20 19:56:51 -0800

    MyVideoPlayer project

---
+++ app/src/main/java/com/example/myvideoplayer/PlayerActivity.kt
@@ -0,0 +1,68 @@
+package com.example.myvideoplayer
+
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import androidx.activity.ComponentActivity
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+
+class PlayerActivity : ComponentActivity() {
+
+    private var player: ExoPlayer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+
+        hideSystemBars()
+
+        val uri: Uri = intent.data ?: run {
+            finish()
+            return
+        }
+
+        val playerView = findViewById<PlayerView>(R.id.playerView)
+
+        player = ExoPlayer.Builder(this).build().also { exo ->
+            playerView.player = exo
+            exo.setMediaItem(MediaItem.fromUri(uri))
+            exo.prepare()
+            exo.playWhenReady = true
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        player?.release()
+        player = null
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemBars()
+    }
+
+    private fun hideSystemBars() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.let { controller ->
+                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                controller.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility =
+                (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+        }
+    }
+}
